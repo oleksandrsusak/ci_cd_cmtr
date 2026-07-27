e03b6bb915f243bd9f92f20f92cb772b6d574453
aadapw7c <aadapw7c@cloud-mentor.local>
2026-07-27 11:28:50 +0000

    ci: verification 20260727112850

--- lambdas/cmtr_aadapw7c_lambda_createOrder/handler.py
+++ lambdas/cmtr_aadapw7c_lambda_createOrder/handler.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
 
     table.put_item(
         Item={
-    "verification": "20260727105609",
+    "verification": "20260727112850",
             "orderId": order_id,
             "userId": "mock-user",
             "productId": "mock-product",
